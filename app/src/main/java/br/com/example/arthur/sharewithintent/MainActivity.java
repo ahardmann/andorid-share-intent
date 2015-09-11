5886c7715c065c452d467049a856aafe95ee4194
Arthur Hardmann <arthur.hardmann@hotmail.com>
2015-09-11 10:38:54 -0300

    shareImage intent

--- app/src/main/java/br/com/example/arthur/sharewithintent/MainActivity.java
+++ app/src/main/java/br/com/example/arthur/sharewithintent/MainActivity.java
@@ -2,7 +2,10 @@ package br.com.example.arthur.sharewithintent;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 
 
@@ -15,10 +18,28 @@ public class MainActivity extends Activity {
     }
 
     public void shareText(View v){
+        Intent sendText = new Intent();
+        sendText.setAction(Intent.ACTION_SEND);
+        sendText.putExtra(Intent.EXTRA_TEXT, "Cheech and Chong!");
+        sendText.setType("text/plain");
+        startActivity(Intent.createChooser(sendText, "Share to..."));
+    }
+
+    public void shareImage(View v){
+
+        Uri uri = null;
+        try{
+            // add <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/> on androidManifest
+            //add image to res/drawable
+            uri = Uri.parse(MediaStore.Images.Media.insertImage(this.getContentResolver(),
+                    BitmapFactory.decodeResource(getResources(), R.drawable.cheech), null, null));
+        }catch (NullPointerException e){
+            e.printStackTrace();
+        }
         Intent sendIntent = new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, "Hello World!");
-        sendIntent.setType("text/plain");
+        sendIntent.putExtra(Intent.EXTRA_STREAM, uri);
+        sendIntent.setType("image/*");
         startActivity(Intent.createChooser(sendIntent, "Share to..."));
     }
 }
